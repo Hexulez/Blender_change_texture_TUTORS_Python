36de4479aaf32c8bb279e19450d0dd20bb6ffd45
Hexulez <henry.juhola@edu.keuda.fi>
2023-03-09 15:33:44 +0200

    HIFK3 toimii

--- Blender_change_texture_TUTORS_Python/Blender_change_texture_TUTORS_Python/Blender_change_texture_TUTORS_Python.py
+++ Blender_change_texture_TUTORS_Python/Blender_change_texture_TUTORS_Python/Blender_change_texture_TUTORS_Python.py
@@ -1,10 +1,11 @@
-from asyncio.windows_events import NULL
 import bpy
 import os
 import glob
 import re
 
-different_name = False
+#merkki
+
+series_number = 1
 
 #function to remove packed images
 def remove_packed_images():
@@ -14,6 +15,57 @@ def remove_packed_images():
     # Remove each packed image from the Blender file
     for img in packed_images:
         bpy.data.images.remove(img)
+
+#def make function to but space middle of string
+def make_space_middle(string):
+    mid = len(string) // 2
+    first_part = string[:mid]
+    second_part = string[mid:]
+    return first_part.lower().capitalize() + " " + second_part.lower().capitalize()
+    
+
+#function to check from subfolder name first part what series we blendering 
+def check_series(first_part_of_sub):
+    if first_part_of_sub.upper() == "RETRO":
+        series_number = 1
+    elif first_part_of_sub.upper() == "HIFK2":
+        series_number = 2
+    elif first_part_of_sub.upper() == "HIFK3":
+        series_number = 3
+
+
+#function to arrenges subfolder name to correct order
+def arrenge_subfolder(subF ,series_number):
+    remake_list = []
+    
+    if series_number == 1:
+        
+        remake_list.append(subF[4])
+        remake_list.append(subF[3])
+        remake_list.append(subF[2])
+        remake_list.append(subF[1])
+        remake_list.append(subF[0])
+        
+        return remake_list
+        
+    elif series_number == 2:
+        return
+        
+    elif series_number == 3:
+        series_number = 3
+        #numero, nimi, rarity, sarja, merkki
+        #number, name, rarity, series, brand
+        remake_list.append(subF[1])
+        remake_list.append(subF[2])
+        remake_list.append(subF[4])
+        remake_list.append(make_space_middle(subF[3]))
+        remake_list.append("22 - 23")
+        
+        
+        return remake_list
+    
+    
+    
 
 # Get the path of the current Blender file
 filepath = bpy.data.filepath
@@ -25,25 +77,44 @@ base_dir = dirpath
 
 # Loop through each directory in the base directory
 for subdir in os.listdir(base_dir):
-    remove_packed_images()
+
+    #make subdir_list variable to split the name of the folder
+    subdir_list = re.split("-|_", subdir)
+
+    #check if series number
+    series_number = check_series(subdir_list[0])
+
+    #arrenge subfolder name to correct order
+    subdir_list = arrenge_subfolder(subdir_list, series_number)
+    
+    
     # Construct the full path of the current directory
     current_dir = os.path.join(base_dir, subdir)
     
-    # Look for the texture file named "Texture01"
-    texture_file = glob.glob(os.path.join(current_dir, "Texture01.*"))
+    #check if series is anniversary
+    if series_number == 1:
+        # Look for the texture file named "Texture01"
+        texture_file = glob.glob(os.path.join(current_dir, "Texture01.*"))
+    else:
+        # look for the texture file named "*_BaseColor
+        texture_file = glob.glob(os.path.join(current_dir, "*_BaseColor.*"))
     
     
-    # If a texture file is found, load it into Blender
+    #if texture file is found, start make new blender file
     if texture_file:
+        # Remove the packed images from the Blender file
+        remove_packed_images()
+        
+        # If a texture file is found, load it into Blender
         img = bpy.data.images.load(texture_file[0])
         
-        #check if there is object with the name
-        if "obj0.002" not in bpy.data.objects:
+        #check if series is anniversary
+        if series_number == 1:
+            obj_name = "obj0.002"
+        else:
             obj_name = "Jersey_Red.001"
             
-        else:
-            # Set the name of the object whose texture we want to change
-            obj_name = "obj0.002"
+        
         
         
         # Get the object by name
@@ -57,16 +128,16 @@ for subdir in os.listdir(base_dir):
 
         # Pack the texture image into the Blender file
         img.pack()
-
+        
         ###
 
-        subdir_list = re.split("-|_", subdir)
-        player_name = subdir_list[4]
+        #set subdir to variables
+        player_name = subdir_list[1]
         player_name = player_name.lower().capitalize()
-        player_number = subdir_list[3]
+        player_number = subdir_list[0]
         rarity = subdir_list[2]
         rarity = rarity.lower().capitalize()
-        series = subdir_list[0].upper() + " "+ subdir_list[1].upper()
+        series = subdir_list[3].upper() + " "+ subdir_list[4].upper()
         
 
 
@@ -75,8 +146,14 @@ for subdir in os.listdir(base_dir):
         # Get the object by name
         obj2 = bpy.data.objects[obj2_name]
 
-        # Change the text of the object
-        obj2.data.body = f"#{player_number} {player_name} - {rarity} Edition\n{series}\nAnniversary Series"
+        #check if series is anniversary
+        if series_number == 1:
+            # Change the text of the object
+            obj2.data.body = f"#{player_number} {player_name} - {rarity} Edition\n{series}\nAnniversary Series"
+        
+        else:
+            # Change the text of the object
+            obj2.data.body = f"#{player_number} {player_name} - {rarity} Edition\n{series}" 
 
         # Save the blend file with a new name
         bpy.ops.wm.save_as_mainfile(filepath=os.path.join(current_dir, "{}.blend".format(subdir)), check_existing=False)
