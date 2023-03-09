9b2d97e6e85bf02a822b4a7d045b9b576b062133
Hexulez <henry.juhola@edu.keuda.fi>
2023-03-09 02:02:10 +0200

    retro ready

--- Blender_change_texture_TUTORS_Python/Blender_change_texture_TUTORS_Python/Blender_change_texture_TUTORS_Python.py
+++ Blender_change_texture_TUTORS_Python/Blender_change_texture_TUTORS_Python/Blender_change_texture_TUTORS_Python.py
@@ -1,6 +1,10 @@
+from asyncio.windows_events import NULL
 import bpy
 import os
 import glob
+import re
+
+different_name = False
 
 #function to remove packed images
 def remove_packed_images():
@@ -11,9 +15,13 @@ def remove_packed_images():
     for img in packed_images:
         bpy.data.images.remove(img)
 
+# Get the path of the current Blender file
+filepath = bpy.data.filepath
 
+# Remove the filename from the path to get the directory path
+dirpath = os.path.dirname(filepath)
 # Set the base directory where the textures are stored
-base_dir = "E:/aaa_harjoittelu/NFT/playground/test"
+base_dir = dirpath
 
 # Loop through each directory in the base directory
 for subdir in os.listdir(base_dir):
@@ -24,13 +32,20 @@ for subdir in os.listdir(base_dir):
     # Look for the texture file named "Texture01"
     texture_file = glob.glob(os.path.join(current_dir, "Texture01.*"))
     
+    
     # If a texture file is found, load it into Blender
     if texture_file:
         img = bpy.data.images.load(texture_file[0])
         
-        # Set the name of the object whose texture we want to change
-        obj_name = "obj0.002"
-
+        #check if there is object with the name
+        if "obj0.002" not in bpy.data.objects:
+            obj_name = "Jersey_Red.001"
+            
+        else:
+            # Set the name of the object whose texture we want to change
+            obj_name = "obj0.002"
+        
+        
         # Get the object by name
         obj = bpy.data.objects[obj_name]
 
@@ -45,18 +60,16 @@ for subdir in os.listdir(base_dir):
 
         ###
 
-        subdir_list = subdir.split("-")
+        subdir_list = re.split("-|_", subdir)
         player_name = subdir_list[4]
         player_name = player_name.lower().capitalize()
         player_number = subdir_list[3]
         rarity = subdir_list[2]
         rarity = rarity.lower().capitalize()
-        series = subdir_list[0] + " "+ subdir_list[1]
+        series = subdir_list[0].upper() + " "+ subdir_list[1].upper()
         
 
 
-
-        #Get the selected object
         obj2_name = "Text.003"
 
         # Get the object by name
